155c6c1fc1422376e42f6ffedf2b5caae918c135
Yahshua Emmanuel George Ball <130876430+yahweh90@users.noreply.github.com>
2025-03-23 03:57:08 -0400

    Update 2-vpc.tf

--- 2-vpc.tf
+++ 2-vpc.tf
@@ -7,7 +7,34 @@ resource "google_compute_network" "be_a_man_vpc" {
 # Create subnet
 resource "google_compute_subnetwork" "be_a_man_subnet" {
   name          = "be-a-man-subnet"
-  ip_cidr_range = "10.0.0.0/24"
+  ip_cidr_range = "10.50.17.0/24"
   region        = "us-central1"
   network       = google_compute_network.be_a_man_vpc.id
+}
+
+# Create firewall
+resource "google_compute_firewall" "allow_ssh_http" {
+  name      = "allow-ssh-http"
+  network   = google_compute_network.be_a_man_vpc.id
+  direction = "INGRESS"
+  priority  = 1000
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh-http-server"]
 }
